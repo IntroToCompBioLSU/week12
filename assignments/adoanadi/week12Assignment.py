5f082b22d595f834d2c28bc64fc88fbe23359357
jembrown <jeremymbrown@gmail.com>
2018-12-11 06:15:05 -0600

    Comments added for adoanadi

--- assignments/adoanadi/week12Assignment.py
+++ assignments/adoanadi/week12Assignment.py
@@ -2,11 +2,11 @@
 
 class sports:
     """A class to store information about sports"""
-
+    
     def __init__(self, group="outdoor", ball=False):
         self.group = group
         self.ball = ball
-
+        
     def summarize(self):
         if (self.ball):
             print("This sport requires the use of a ball and is an % sport." % (self.group))
@@ -15,13 +15,20 @@ class sports:
 
 class leisure(sports):
     """A class to store information about leisure activities that are sports."""
-
-    def __init(self, name="", leisure=False):
+    
+    def __init__(self, name="", leisure=True):	# DB: Need 2nd __ around init -- also maybe set leisure to True in this case?
         sports.__init__(self)
         self.name = name
         self.leisure = leisure
-
+        
     def summarize(self):
         sports.summarize(self)
         print("This is a leisure activity.")
-        print
+        
+# DB: Aside from a couple of minor formatting changes, this looks good! Here's the output
+#     I get when using this code:
+#
+#     >>> myLeisureSport = leisure()
+#     >>> myLeisureSport.summarize()
+#     This sport does not require the use of a ball and is an outdoorport.
+#     This is a leisure activity.
--- assignments/adoanadi/week12TryExcept.py
+++ assignments/adoanadi/week12TryExcept.py
@@ -3,6 +3,8 @@
 # try...except statement for input of numbers 
 try:
     myNumber = input("Enter your number here: ") # user prompted to enter any number
-    assert (myNumber >= 50) # Testing that number is greater than/equal to 50
+    assert (int(myNumber) >= 50) # Testing that number is greater than/equal to 50
 except AssertionError: # If the number is not greater than/equal to 50
     print("This number is too small. Try again. ") # This message is printed
+    
+# DB: Need to convert myNumber to an integer in the assert statement, but otherwise good!
