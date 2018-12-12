a8e6738007dc7fb307da02be44b75bf18274bc49
jembrown <jeremymbrown@gmail.com>
2018-12-12 02:48:31 -0600

    Comments for pphil17

--- assignments/pphil17/classInheritance.py
+++ assignments/pphil17/classInheritance.py
@@ -3,15 +3,17 @@
 Class sports:
     """A class to store information about sports."""
 
-    def init(self, group = "hobby", olympicSport=True):
-    self.group = group
-    self.olympicSports = olympicSports
+	# DB: Some indentation errors in this definition.
 
-def summerize(self):
-    if self.olympicSport:
-       print("This sport is an Olympic sport, and is a %s." % (self.group))
-    else:
-       print("This sport is not an Olympic sport, but is a %s." % (self.group))
+    def init(self, group = "hobby", olympicSport=True):
+		self.group = group
+		self.olympicSports = olympicSports
+
+	def summerize(self):
+		if self.olympicSport:
+		   print("This sport is an Olympic sport, and is a %s." % (self.group))
+		else:
+		   print("This sport is not an Olympic sport, but is a %s." % (self.group))
 
 class ball(sports):
     """A class to store information about sports that use balls."""
@@ -25,3 +27,5 @@ class ball(sports):
           print("%s is a sport that uses a ball.")
        else:
           print("%s is a sport that does not use a ball.")
+
+# DB: Looks good! Did you attempt a try...except block?
