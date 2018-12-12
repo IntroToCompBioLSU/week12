a5eb79c0e76204cacb41ef81149bd1c4c495dd4e
jembrown <jeremymbrown@gmail.com>
2018-12-12 02:31:14 -0600

    ClassWeek12.py

--- assignments/whurst1/ClassWeek12.py
+++ assignments/whurst1/ClassWeek12.py
@@ -5,7 +5,7 @@ class Car:
 	"""This is a class to store information about any type of Car"""
 	def __init__(self, Type=" ", Model=""):
 		self.Type = Type
-		self.Model = Mdel
+		self.Model = Model	# DB: Spelling error
 
 	def summarize(self):
 		if (self.Model) == "SUV":
@@ -17,7 +17,7 @@ class Brand(Car):
 	"""This is a class used to store information on what brand the car is."""
 
 	def __init__(self, Brand=" ", Model="SUV", Size="Large Vehicle"):
-		clothes.__init__(self)
+		Car.__init__(self)	# DB: clothes --> Car
 		self.Brand = Brand
 		self.Model = Model
 		self.Size = Size
@@ -25,12 +25,12 @@ class Brand(Car):
 		Car.summarize(self)
 		print("This Car is a %s, %s from the brand %s." %( self.Size, self.Model, self.Brand))
 #Class that shares properties with the car.
-class Cost(car):
+class Cost(Car):	# DB: capitalization error
 
         """This is a class used to store information on Cost of Cars"""
 
         def __init__(self, Cost=" ", Model="SUV", Size="Large Vehicle"):
-                clothes.__init__(self)
+                Car.__init__(self)	# DB: clothes --> Car
                 self.Cost = Cost
                 self.Size = Size
         def summarize(self):
@@ -40,3 +40,8 @@ class Cost(car):
 #defining the items in the class
 CarOne = Brand(Brand="Chevy", Size="Large", Model="SUV",)
 CarOne = Cost(Cost="Expensive")
+
+# DB: I like this example a lot. A few things: - Typically we use derived classes to define
+#     more specific cases, and not attributes of the base class. So if Car is your base,
+#     a derived class could be Ford, GM, Sports, SUV, Expensive, Cheap, etc. - There were 
+#     a few spelling and syntax errors - Did you attempt a try...except block?
